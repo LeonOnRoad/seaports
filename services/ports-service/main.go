2fec12b0283490bc8893376fe72657b6e3482294
LeonOnRoad <cristianis78@gmail.com>
2021-01-11 17:26:18 +0200

    ports-service: graceful shutdown

--- services/ports-service/main.go
+++ services/ports-service/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "company.com/seaports/services/ports-service/config"
+import (
+	"context"
+
+	pkgService "company.com/seaports/pkg/service"
+
+	"company.com/seaports/services/ports-service/config"
+)
 
 func main() {
-	config.LoadConfig()
+	cfg := config.LoadConfig()
+	sh := pkgService.NewShutdownHandler(cfg.ShutdownTimeout)
+	defer sh.Close()
+
+	closeFunc := func(ctx context.Context) error {
+
+		// add here other resources which need to be close
+
+		return nil
+	}
+
+	sh.WaitShutdown(closeFunc) // blocking
 }
