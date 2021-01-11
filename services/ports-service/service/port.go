68820c1b61c8a19a085bba7abf676b88f0162086
LeonOnRoad <cristianis78@gmail.com>
2021-01-11 17:41:26 +0200

    ports-service: implement get port in service layer

--- services/ports-service/service/port.go
+++ services/ports-service/service/port.go
@@ -2,8 +2,13 @@ package service
 
 import (
 	"context"
+	"encoding/json"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	pbPort "company.com/seaports/proto/src/api/port"
+
 	"company.com/seaports/services/ports-service/repository"
 )
 
@@ -22,9 +27,39 @@ func NewPort(r repository.Repository) *Port {
 }
 
 func (s Port) GetPort(ctx context.Context, req *pbPort.GetPortRequest) (*pbPort.Port, error) {
-	return nil, nil
+	val, rErr := s.repo.Get(ctx, req.Id)
+	if rErr != nil {
+		return nil, convertRepoErrorToGrpcError(rErr)
+	}
+
+	port := &pbPort.Port{}
+	err := json.Unmarshal([]byte(val), port)
+	if err != nil {
+		// log failure of unmarshal
+		return nil, status.Error(codes.Internal, "Failed to get port")
+	}
+	return port, nil
 }
 
 func (s Port) StreamImportedPorts(stream pbPort.PortsService_StreamImportedPortsServer) error {
 	return nil
+}
+
+func convertRepoErrorToGrpcError(re *repository.RepoError) error {
+	if re == nil {
+		return nil
+	}
+
+	switch re.Type {
+	case repository.NONE:
+		return status.Error(codes.OK, re.Err.Error())
+	case repository.NOT_FOUND:
+		return status.Error(codes.NotFound, re.Err.Error())
+	case repository.ALREADY_EXISTS:
+		return status.Error(codes.AlreadyExists, re.Err.Error())
+	case repository.INTERNAL:
+		return status.Error(codes.Internal, re.Err.Error())
+	default:
+		return status.Error(codes.Internal, re.Err.Error())
+	}
 }
